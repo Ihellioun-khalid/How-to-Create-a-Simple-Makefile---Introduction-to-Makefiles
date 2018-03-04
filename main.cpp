97ae249785fe2f93f3e9f7af9fcff7d88a0063e3
Khalid you <k.ihellioun@ump.ac.ma>
2018-03-04 20:11:12 +0100

    youtube Paul Programming

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+* @Author: ihelliounkhalid
+* @Date:   2018-03-04 19:43:57
+* @Last Modified by:   ihelliounkhalid
+* @Last Modified time: 2018-03-04 19:49:35
+*/
+#include "message.h"
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	message m;
+	m.printMessage();
+
+	return 0;
+}
---
+++ message.cpp
@@ -0,0 +1,14 @@
+/*
+* @Author: ihelliounkhalid
+* @Date:   2018-03-04 19:44:09
+* @Last Modified by:   ihelliounkhalid
+* @Last Modified time: 2018-03-04 20:08:02
+*/
+#include <iostream>
+#include "message.h"
+
+using namespace std;
+
+void message::printMessage(){
+	cout << "IHELLIOUN Khalid \n";
+}
